3b9ebd0c44075e53a549f5c3f24bdca5025482ca
Yousef Elghanam <yousef.elghanam@gmail.com>
2022-12-24 12:31:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd
+from utils import apology, login_required, lookup, usd
 
 # Configure application
 app = Flask(__name__)
