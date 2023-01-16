bf1e3fe0e40cf52a87bcb5c21704fb42c0a68061
Yousef Elghanam <yousef.elghanam@gmail.com>
2023-01-16 09:42:53 +0200

    Revert "day11"
    This reverts commit 24b131ab6ecb2358e99a69e742421e3f4d83893e.

--- quizsef/templates/myScripts.js
+++ quizsef/templates/myScripts.js
@@ -35,16 +35,12 @@
                     this.style.height = "auto";
                     this.style.height = `${this.scrollHeight}px`;
                 });
-                const rightAnswer{{row+1}} = document.getElementById("rightanswer{{row+1}}");
-                rightAnswer{{row+1}}.style.cssText = `height: ${rightAnswer{{row+1}}.scrollHeight}px; overflow-y: hidden`;
+                const rightAnswer = document.getElementById("rightanswer");
+                rightAnswer.style.cssText = `height: ${rightAnswer.scrollHeight}px; overflow-y: hidden`;
 
-                rightAnswer{{row+1}}.addEventListener("input", function(){
+                rightAnswer.addEventListener("input", function(){
                     this.style.height = "auto";
                     this.style.height = `${this.scrollHeight}px`;
                 });
 
-                function copy(id) {
-                    let textarea = document.getElementById(id);
-                    textarea.select();
-                    document.execCommand("copy");
-                  }
+            
